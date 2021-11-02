a16b0cf7cf1ff09a84d55e3a0c0e3134c7361933
MateiEne <ene.matei21@gmail.com>
2021-11-02 18:00:17 +0200

    added convertion button who can convert the amount

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+const EURO_CURRENCY = 4.5;
+double value = 0;
 
 void main() {
   runApp(const CurrencyConvertorApp());
 }
 
 class CurrencyConvertorApp extends StatelessWidget {
-  const CurrencyConvertorApp({Key? key}) : super(key: key);
+   const CurrencyConvertorApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,13 +18,39 @@ class CurrencyConvertorApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.red,
-          title: Center(
+          title: const Center(
               child: Text('Currency Convertor')
           ),
         ),
-        body: new Column(
-          children: [
+        body: Column(
+          children: <Widget>[
             Image.asset('assets/cash-ron.png'),
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Enter the amount in EUR',
+              ),
+              keyboardType: TextInputType.number,
+              onChanged: (String text) {
+                value = double.parse(text);
+              },
+              onSubmitted: (text) {
+                value = value * EURO_CURRENCY;
+                print(value);
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ElevatedButton(
+                  onPressed: () {
+                    value = value * EURO_CURRENCY;
+                    print(value);
+                  },
+                child: const Text('Convert'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.grey),
+                ),
+              ),
+            ),
           ],
         ),
       ),
