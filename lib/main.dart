28ec878aa12945f7ef99afbd74a1c180d731b615
MateiEne <ene.matei21@gmail.com>
2021-11-03 12:35:52 +0200

    added empty value validator

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 const EURO_CURRENCY = 4.5;
-double value = 0;
+double valReceived = 0;
+double valueConverted = 0;
+
+final GlobalKey<FormState> formkey = GlobalKey<FormState>();
 
 void main() {
   runApp(const CurrencyConvertorApp());
 }
 
 class CurrencyConvertorApp extends StatelessWidget {
-   const CurrencyConvertorApp({Key? key}) : super(key: key);
+  const CurrencyConvertorApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -25,30 +28,41 @@ class CurrencyConvertorApp extends StatelessWidget {
         body: Column(
           children: <Widget>[
             Image.asset('assets/cash-ron.png'),
-            TextField(
-              decoration: const InputDecoration(
-                hintText: 'Enter the amount in EUR',
-              ),
-              keyboardType: TextInputType.number,
-              onChanged: (String text) {
-                value = double.parse(text);
-              },
-              onSubmitted: (text) {
-                value = value * EURO_CURRENCY;
-                print(value);
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: ElevatedButton(
-                  onPressed: () {
-                    value = value * EURO_CURRENCY;
-                    print(value);
-                  },
-                child: const Text('Convert'),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.grey),
-                ),
+            Form(
+              key: formkey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Enter the amount in EUR',
+                    ),
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a number';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      valReceived = double.parse(value);
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (formkey.currentState!.validate()) {
+                          valueConverted = valReceived * EURO_CURRENCY;
+                          print(valueConverted);
+                        }
+                      },
+                      child: const Text('Convert'),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.grey),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
