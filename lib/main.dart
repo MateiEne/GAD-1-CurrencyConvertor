88085f975224187eff4a74e6b7b9f9593e47d940
MateiEne <ene.matei21@gmail.com>
2021-11-04 22:14:33 +0200

    changed it in stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 const EURO_CURRENCY = 4.5;
-double valReceived = 0;
+double valueReceived = 0;
 double valueConverted = 0;
 
 final GlobalKey<FormState> formkey = GlobalKey<FormState>();
@@ -18,53 +18,73 @@ class CurrencyConvertorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: const Center(
-              child: Text('Currency Convertor')
-          ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController controller = TextEditingController();
+
+  double value = 0;
+  final double EURO_CURRENCY = 4.5;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Currency Convertor',
         ),
-        body: Column(
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
           children: <Widget>[
-            Image.asset('assets/cash-ron.png'),
-            Form(
-              key: formkey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: 'Enter the amount in EUR',
-                    ),
-                    keyboardType: TextInputType.number,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a number';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) {
-                      valReceived = double.parse(value);
-                    },
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (formkey.currentState!.validate()) {
-                          valueConverted = valReceived * EURO_CURRENCY;
-                          print(valueConverted);
-                        }
-                      },
-                      child: const Text('Convert'),
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(Colors.grey),
-                      ),
-                    ),
-                  ),
-                ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/cash-ron.png',
+                  height: 200,
+                ),
+              ],
+            ),
+            TextField(
+              controller: controller,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                hintText: 'Enter a number',
+                suffix: IconButton(
+                  onPressed: () {
+                    controller.clear();
+                  },
+                  icon: const Icon(Icons.close),
+                ),
               ),
             ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  value = double.parse(controller.text);
+                  value = value * EURO_CURRENCY;
+                });
+                print(value.toStringAsFixed(2));
+              },
+              child: const Text('Convert!'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.grey),
+              ),
+            ),
+            Text('$value'),
           ],
         ),
       ),
